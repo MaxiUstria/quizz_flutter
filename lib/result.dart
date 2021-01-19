c1cf767a2596472f9d61999aaf53f95c49e3a329
Maximiliano Ustria <maxiustria@MacBook-Pro-de-maxi.local>
2021-01-19 01:57:14 -0300

    first commit

---
+++ lib/result.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    if (resultScore <= 8) {
+      return 'You are awesome!';
+    } else {
+      return 'Meh';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text('Restart'),
+            textColor: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+}
